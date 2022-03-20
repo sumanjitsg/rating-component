fe56292153706e8d057d22b4ba7ba5250ddccc2f
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2022-03-20 15:36:17 +0530

    added rating selection code

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const form = document.querySelector('form');
+let currentRating = document.querySelector('.selected');
 
 function handleSubmit(e) {
+  console.log(e);
   e.preventDefault();
-  console.log('submitted');
+}
+
+function setRating({ target }) {
+  const selectedRating = target.labels[0];
+
+  currentRating.classList.remove('selected');
+  selectedRating.classList.add('selected');
+
+  currentRating = selectedRating;
+
+  // needed?
+  // e.preventDefault();
 }
 
 // Register Event Listeners
 form.addEventListener('submit', handleSubmit);
+form.addEventListener('change', setRating);
