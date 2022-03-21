fc47bfba068d98435621558256f44c94a73b7652
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2022-03-22 01:53:52 +0530

    remove hidden class from rating element

--- thankyou.js
+++ thankyou.js
@@ -1,8 +1,8 @@
 const ratingContainer = document.querySelector('.line > p');
 const rating = window.sessionStorage.getItem('rating');
 
-if (rating === null) {
-  ratingContainer.parentNode.style.display = 'none';
-} else {
+if (rating !== null) {
   ratingContainer.textContent = `You selected ${rating} out of 5`;
+
+  ratingContainer.parentElement.classList.remove('hidden');
 }
