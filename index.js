e011f87b4b93c7e33ff10d35832ff65ae07d9043
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2022-03-30 20:06:50 +0530

    radio buttons keyboard accessible changes

--- index.js
+++ index.js
@@ -9,23 +9,9 @@ function handleSubmit(e) {
 
   window.sessionStorage.setItem('rating', rating);
 
-  // rating doesn't reset to 5 on reload. resets on hard reload (browser caching?)
-
+  // replace allows not going back to feedback form page
   window.location.replace('thankyou.html');
-}
-
-function setRating({ target }) {
-  const selectedRating = target.labels[0];
-
-  currentRating.classList.remove('selected');
-  selectedRating.classList.add('selected');
-
-  currentRating = selectedRating;
-
-  // needed?
-  // e.preventDefault();
 }
 
 // Register Event Listeners
 form.addEventListener('submit', handleSubmit);
-form.addEventListener('change', setRating);
--- thankyou.js
+++ thankyou.js
@@ -3,8 +3,8 @@
 const ratingContainer = document.querySelector('.line > p');
 const rating = window.sessionStorage.getItem('rating');
 
+// only when rating set in storage (possibly from feedback form page)
 if (rating !== null) {
   ratingContainer.textContent = `You selected ${rating} out of 5`;
-
   ratingContainer.parentElement.classList.remove('hidden');
 }
