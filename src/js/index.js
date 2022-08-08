b60503eafaaee6e971a705dfed74f742fa81420e
Sumanjit Sengupta <sumanjitsengupta08@gmail.com>
2022-08-08 11:55:26 +0530

    moved thank you page to index template

--- src/js/index.js
+++ src/js/index.js
@@ -14,9 +14,27 @@ function handleSubmit(e) {
   const formData = new FormData(ratingForm);
   const rating = formData.get("rating");
 
-  // set selected rating in session storage
-  window.sessionStorage.setItem("rating", rating);
+  // get content container
+  const contentContainerEl = document.querySelector("[data-content-container]");
+
+  // get thankyou template
+  const thankyouTemplate = document.querySelector("[data-thankyou-template]");
+
+  // get rating text
+  const selectedRatingText = thankyouTemplate.content.querySelector(
+    "[data-selected-rating-text]"
+  );
+
+  // display rating if not null
+  if (rating !== null) {
+    selectedRatingText.append(`You selected ${rating} out of 5`);
+    selectedRatingText.classList.remove("hidden");
+  }
+
+  contentContainerEl.replaceChildren(
+    thankyouTemplate.content.firstElementChild.cloneNode(true)
+  );
 
   // go to thankyou page
-  window.location.replace("thankyou.html");
+  // window.location.replace("thankyou.html");
 }
